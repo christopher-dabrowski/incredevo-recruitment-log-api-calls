11ed26c9f20af30b632223068c866cea79d19a3b
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2023-04-05 13:30:19 +0200

    Move trigger metadata to dedicated file

--- functionApp/StoreApiResponse.cs
+++ functionApp/StoreApiResponse.cs
@@ -14,26 +14,10 @@ namespace Function
         }
 
         [Function("StoreApiResponse")]
-        public void Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)] MyInfo myTimer)
+        public void Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)] TimerTriggerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
         }
-    }
-
-    public class MyInfo
-    {
-        public MyScheduleStatus ScheduleStatus { get; set; }
-
-        public bool IsPastDue { get; set; }
-    }
-
-    public class MyScheduleStatus
-    {
-        public DateTime Last { get; set; }
-
-        public DateTime Next { get; set; }
-
-        public DateTime LastUpdated { get; set; }
     }
 }
