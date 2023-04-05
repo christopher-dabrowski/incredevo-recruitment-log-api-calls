b24193fa4485ae68afb0285fdb4b7fff2e36b155
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2023-04-05 14:09:38 +0200

    Unify namespaces

--- functionApp/ListLogs.cs
+++ functionApp/ListLogs.cs
@@ -1,38 +1,35 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace Function
+namespace functionApp;
+
+public class ListLogs
 {
-    public class ListLogs
+    private readonly ILogger _logger;
+
+    public ListLogs(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
+        _logger = loggerFactory.CreateLogger<ListLogs>();
+    }
 
-        public ListLogs(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<ListLogs>();
-        }
+    [OpenApiOperation(operationId: "greeting", tags: new[] { "greeting" }, Summary = "Greetings", Description = "This shows a welcome message.", Visibility = OpenApiVisibilityType.Important)]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Summary = "The response", Description = "This returns the response")]
+    [Function("ListLogs")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-        [OpenApiOperation(operationId: "greeting", tags: new[] { "greeting" }, Summary = "Greetings", Description = "This shows a welcome message.", Visibility = OpenApiVisibilityType.Important)]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Summary = "The response", Description = "This returns the response")]
-        [Function("ListLogs")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+        response.WriteString("Welcome to Azure Functions!");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
-        }
+        return response;
     }
 }
--- functionApp/StoreApiResponse.cs
+++ functionApp/StoreApiResponse.cs
@@ -1,23 +1,20 @@
-using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace Function
+namespace functionApp;
+
+public class StoreApiResponse
 {
-    public class StoreApiResponse
+    private readonly ILogger _logger;
+
+    public StoreApiResponse(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
+        _logger = loggerFactory.CreateLogger<StoreApiResponse>();
+    }
 
-        public StoreApiResponse(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<StoreApiResponse>();
-        }
-
-        [Function("StoreApiResponse")]
-        public void Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)] TimerTriggerInfo myTimer)
-        {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-        }
+    [Function("StoreApiResponse")]
+    public void Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)] TimerTriggerInfo myTimer)
+    {
+        _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
     }
 }
