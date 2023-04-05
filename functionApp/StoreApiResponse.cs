44171b83da8bb00ced6014eda0214c5861509e03
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2023-04-05 14:24:52 +0200

    Run request to public api

--- functionApp/StoreApiResponse.cs
+++ functionApp/StoreApiResponse.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using functionApp.httpClients;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -6,15 +8,28 @@ namespace functionApp;
 public class StoreApiResponse
 {
     private readonly ILogger _logger;
+    private readonly IPublicApisHttpClient _publicApisHttpClient;
 
-    public StoreApiResponse(ILoggerFactory loggerFactory)
+    public StoreApiResponse(ILoggerFactory loggerFactory, IPublicApisHttpClient publicApisHttpClient)
     {
+        _publicApisHttpClient = publicApisHttpClient;
         _logger = loggerFactory.CreateLogger<StoreApiResponse>();
     }
 
     [Function("StoreApiResponse")]
-    public void Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)] TimerTriggerInfo myTimer)
+    public async Task Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)] TimerTriggerInfo myTimer)
     {
         _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        try
+        {
+            var apiResponse = await _publicApisHttpClient.GetRanomApiInfo();
+            _logger.LogInformation(JsonSerializer.Serialize(apiResponse));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
